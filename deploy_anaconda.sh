7bf9a0aa2c43b247de70fe953705e18fbd2d090d
Shankar Kulumani <shanks.k@gmail.com>
2018-12-07 12:06:40 -0500

    Add conda deploy script

---
+++ deploy_anaconda.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# this script uses the ANACONDA_TOKEN env var. 
+# to create a token:
+# >>> anaconda login
+# >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
+set -e
+PACKAGENAME=kinematics
+
+echo "Converting conda package..."
+conda convert --platform all $HOME/conda-build/${PACKAGENAME}-*.tar.bz2 --output-dir ${HOME}/conda_platforms/
+
+echo "Deploying to Anaconda.org..."
+anaconda -t $ANACONDA_TOKEN upload ${HOME}/conda_platforms/**/${PACKAGENAME}-*.tar.bz2
+
+echo "Successfully deployed to Anaconda.org."
+exit 0
