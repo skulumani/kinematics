9ebb9068df0affe71fa4335b8ddc1a5e3eec5f7a
Shankar Kulumani <shanks.k@gmail.com>
2018-12-07 12:10:51 -0500

    Update meta data

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Kinematics'
-copyright = '2017, Shankar Kulumani'
+copyright = '2018, Shankar Kulumani'
 author = 'Shankar Kulumani'
 
 # The version info for the project you're documenting, acts as replacement for
